9b61dd4976b5fbd0719225e3a929930b9248aaa2
enricmartos <enric.martos@gmail.com>
2020-01-26 19:52:31 +0100

    Define MediaConverterService endpoints

--- contract/src/main/java/org/emartos/mediaconverterapi/v1/MediaConverterService.java
+++ contract/src/main/java/org/emartos/mediaconverterapi/v1/MediaConverterService.java
@@ -1,33 +1,27 @@
 package org.emartos.mediaconverterapi.v1;
 
 import org.emartos.mediaconverterapi.v1.exceptions.BadRequestException;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HashMap;
 
 
+//@RestController
 @RequestMapping(MediaConverterServiceApi.MEDIA_CONVERTER_API_PATH + MediaConverterServiceApi.VERSION)
 public interface MediaConverterService {
 
-    /**
-     * Returns the image resized
-     *
-     * @param apiKey
-     * @param form image uploaded as a form
-     * @return image resized
-     * @throws BadRequestException
-     */
-//    @Consumes(MediaType.MULTIPART_FORM_DATA)
-//    @Produces("image/jpeg")
-//    @PostMapping("/image/resize")
-//    Response resizeImage(@RequestHeader("apiKey") String apiKey, @MultipartForm ResizeFileUploadForm form)
-//            throws BadRequestException;
+    @PostMapping("/image/resize")
+    ResponseEntity<Resource> resizeImage(@RequestHeader("apiKey") String apiKey, @RequestParam("selectedFile") MultipartFile file,
+                                         @RequestParam("width") Integer width, @RequestParam("height") Integer height)
+            throws BadRequestException;
+
+    @PostMapping("/image/autorotate")
+    ResponseEntity<Resource> autorotateImage(@RequestHeader("apiKey") String apiKey,
+                                             @RequestParam("selectedFile") MultipartFile file) throws BadRequestException;
 
     @GetMapping("/health-check")
     HashMap<String, String> indexApi();
-
-    @PostMapping("/file/upload")
-    void resizeImage(@RequestHeader("apiKey") String apiKey, @RequestParam("file") MultipartFile file)
-            throws BadRequestException;
 }
