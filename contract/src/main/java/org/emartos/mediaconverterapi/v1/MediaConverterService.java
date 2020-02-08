facd6d8f9d8e87ca609ec12dc71b7836a3b00d4e
enricmartos <enric.martos@gmail.com>
2020-02-08 19:52:48 +0100

    Define MediaConverterService endpoints

--- contract/src/main/java/org/emartos/mediaconverterapi/v1/MediaConverterService.java
+++ contract/src/main/java/org/emartos/mediaconverterapi/v1/MediaConverterService.java
@@ -1,5 +1,7 @@
 package org.emartos.mediaconverterapi.v1;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.emartos.mediaconverterapi.v1.exceptions.BadRequestException;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
@@ -13,13 +15,24 @@ import java.util.HashMap;
 public interface MediaConverterService {
 
     @PostMapping("/image/resize")
-    ResponseEntity<Resource> resizeImage(@RequestHeader("apiKey") String apiKey, @RequestParam("selectedFile") MultipartFile file,
-                                         @RequestParam("width") Integer width, @RequestParam("height") Integer height)
+    @ApiOperation(value = "Resizes an image", notes = "Provide an image and the desired width and height")
+    ResponseEntity<Resource> resizeImage(@ApiParam(value = "Api key value")
+            @RequestHeader("apiKey") String apiKey,
+            @ApiParam(value = "Input image")
+            @RequestParam("selectedFile") MultipartFile file,
+            @ApiParam(value = "Desired width")
+            @RequestParam("width") Integer width,
+            @ApiParam(value = "Desired height")
+            @RequestParam("height") Integer height)
             throws BadRequestException;
 
     @PostMapping("/image/autorotate")
-    ResponseEntity<Resource> autorotateImage(@RequestHeader("apiKey") String apiKey,
-                                             @RequestParam("selectedFile") MultipartFile file) throws BadRequestException;
+    @ApiOperation(value = "Autorotates an image", notes = "Provide an image")
+    ResponseEntity<Resource> autorotateImage(
+            @ApiParam(value = "Api key value")
+            @RequestHeader("apiKey") String apiKey,
+            @ApiParam(value = "Input image")
+            @RequestParam("selectedFile") MultipartFile file) throws BadRequestException;
 
     @GetMapping("/health-check")
     HashMap<String, String> healthCheck();
