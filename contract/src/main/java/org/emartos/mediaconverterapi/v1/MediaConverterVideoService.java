2743b26d365120761874bf608e76fde6bd43c91d
enricmartos <enric.martos@gmail.com>
2020-05-01 13:27:12 +0200

    add trim video endpoint

---
+++ contract/src/main/java/org/emartos/mediaconverterapi/v1/MediaConverterVideoService.java
@@ -0,0 +1,26 @@
+package org.emartos.mediaconverterapi.v1;
+
+import org.emartos.mediaconverterapi.v1.exceptions.BadRequestException;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+
+
+@RequestMapping(MediaConverterServiceApi.MEDIA_CONVERTER_API_PATH + MediaConverterServiceApi.VERSION)
+public interface MediaConverterVideoService {
+
+    @PostMapping("/video/trim")
+    ResponseEntity<Resource> trimVideo(@RequestHeader("apiKey") String apiKey, @RequestParam("selectedFile") MultipartFile file,
+                                         @RequestParam("startMinute") Integer startMinute, @RequestParam("startSecond") Integer startSecond,
+                                         @RequestParam("endMinute") Integer endMinute, @RequestParam("endSecond") Integer endSecond)
+
+            throws BadRequestException;
+
+}
